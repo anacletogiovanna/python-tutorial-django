f90b067225d777eda785b04d36079c6e78112ad3
giovannaanacleto <giovanna18batalha@gmail.com>
2019-03-31 20:54:33 -0300

    Tutorial02 - implement the database, models, migrate and admin page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls')), #we can use to polls.site.urls
     path('admin/', admin.site.urls),
 ]
