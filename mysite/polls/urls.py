2cc07210e07d95a744b31bcff0ad1d35716d2e9c
giovannaanacleto <giovanna18batalha@gmail.com>
2019-04-03 22:31:36 -0300

    implement half of tutorial 03

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /polls/
+    path('', views.index, name='index'),
+
+    #ex: /polls/5/ - example url to get a question_id
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    #ex: /polls/id/result - example url to get a result 
+    path('<int:question_id>/results/', views.results, name="results"),
+
+    #ex: polls/5/vote - example url to get a vote
+    path('<int:question_id>/vote/', views.vote, name="vote"),
 ]
